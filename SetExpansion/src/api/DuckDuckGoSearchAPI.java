2f7bf3816bd9c0e33371a2455c46fdf2c23eb9fc
mrugani <mruganikurtadikar@gmail.com>
2015-03-22 17:52:07 +0530

    Added class file for DuckDuckGO

---
+++ SetExpansion/src/api/DuckDuckGoSearchAPI.java
@@ -0,0 +1,12 @@
+package api;
+
+
+public class DuckDuckGoSearchAPI extends SearchAPI{
+
+	@Override
+	public void getTopURLs(String query, int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
